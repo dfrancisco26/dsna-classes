e98ae4dafa69235b80cfe984220ff6a95a4d2d04
jenna-graham <jclayto2@gmail.com>
2022-08-09 12:16:38 -0700

    Queue class enqueue adding item to the back

--- index.js
+++ index.js
@@ -14,6 +14,21 @@ class Stack {
 
 // const instanceOfAStack = new Stack(["cat", "dog", "mouse"]);
 
-class Queue {}
+class Queue {
+  #list;
+
+  constructor(newList) {
+    this.#list = newList || [];
+  }
+  enqueue(addOn) {
+    return this.#list.push(addOn);
+  }
+  get count() {
+    return this.#list.length;
+  }
+  get next() {
+    return this.#list[0];
+  }
+}
 
 module.exports = { Stack, Queue };
--- index.test.js
+++ index.test.js
@@ -1,4 +1,6 @@
 const { Stack } = require("./index.js");
+const { Queue } = require("./index.js");
+
 describe("Stack Class", () => {
   it("#push should add a new item to the stack ", () => {
     const stack = new Stack();
@@ -6,4 +8,13 @@ describe("Stack Class", () => {
     expect(stack.peek()).toEqual(1);
   });
   // add more tests here...
+});
+describe("Queue Class", () => {
+  it("#enqueue should add an item to the back", () => {
+    const queue = new Queue();
+    queue.enqueue("fox");
+    queue.enqueue("goose");
+    expect(queue.count).toBe(2);
+    expect(queue.next).toBe("fox");
+  });
 });
