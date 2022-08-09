b4f7e85dac4e7231f162a902ff935820d1fa6f52
jenna-graham <jclayto2@gmail.com>
2022-08-09 10:51:54 -0700

    .push adds new item to the stack'

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
-class Stack {}
+class Stack {
+  #list = [];
+
+  constructor(theList) {
+    if (theList) this.#list = theList;
+  }
+  push(newItem) {
+    this.#list.push(newItem);
+  }
+  peek() {
+    return this.#list[this.#list.length - 1];
+  }
+}
+
+// const instanceOfAStack = new Stack(["cat", "dog", "mouse"]);
 
 class Queue {}
 
